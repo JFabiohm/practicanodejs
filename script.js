e81de54bf130f66e36e28cc9e1128c05933827fc
JFabiohm <kawaighm@gmail.com>
2024-08-13 11:40:15 -0600

    practica de nodejs

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ class Auto {
         this.marca=marca;
         this.modelo=modelos;
         this.year=year;
-        
+    
     }
     mostrarAuto(){
-        console.log( ´este vehiculo es $(this.marca)´)
+       return console.log( `este vehiculo es ${this.marca} ${this.modelo} ${this.year}`)
     }
 }
+
+const miCarroToyota = new  Auto('Toyota', 'Corolla', 2024);
+const miCarroHyunday = new  Auto('Hyunday', 'Elantra', 2024);
