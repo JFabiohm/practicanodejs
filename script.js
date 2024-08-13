76c1cc6ff1844e34fdc7597b6ce0d668a590d203
JFabiohm <kawaighm@gmail.com>
2024-08-13 11:24:45 -0600

    practica

---
+++ script.js
@@ -0,0 +1,11 @@
+class Auto {
+    constructor(marca, modelo, year) {
+        this.marca=marca;
+        this.modelo=modelos;
+        this.year=year;
+        
+    }
+    mostrarAuto(){
+        console.log( ´este vehiculo es $(this.marca)´)
+    }
+}
